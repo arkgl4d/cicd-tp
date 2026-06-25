8cacc1aa9371aacd0206779a835ca8d330f4dcfd
root <arkgl4d@gmail.com>
2026-06-25 13:23:51 +0200

    commit

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key                     = "test"
-  secret_key                     = "test"
-  region                         = "us-east-1"
-  s3_use_path_style              = true
-  skip_credentials_validation    = true
-  skip_metadata_api_check        = true
-  skip_requesting_account_id     = true
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
   endpoints {
     apigateway     = "http://localhost:4566"
